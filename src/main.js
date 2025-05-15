e158c1e1252313625add5fd3616d7d5c355dc782
Mingyang Han <Mingyang_Han@esri.com>
2025-05-15 14:59:34 +0800

    other: try dangerouslyAllowBrowser

--- src/main.js
+++ src/main.js
@@ -27,10 +27,7 @@ if (USE_AZURE) {
   if(!AZURE_API_VERSION){
     throw new Error("Azure API version is required when `USE_AZURE` is true.")
   }
-  console.log('AZURE_ENDPOINT', AZURE_ENDPOINT)
-  console.log('AZURE_API_VERSION', AZURE_API_VERSION)
-  console.log('OPENAI_API_KEY', OPENAI_API_KEY)
-  openai = new AzureOpenAI({ endpoint: AZURE_ENDPOINT, apiVersion: AZURE_API_VERSION, apiKey: OPENAI_API_KEY })
+  openai = new AzureOpenAI({ endpoint: AZURE_ENDPOINT, apiVersion: AZURE_API_VERSION, apiKey: OPENAI_API_KEY, dangerouslyAllowBrowser: true })
 } else {
   openai = new OpenAI({ apiKey: OPENAI_API_KEY })
 }
